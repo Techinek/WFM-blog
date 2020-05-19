1ccc205b1438093a8f7ad36289924ea1aa058953
techinek <techinek@gmail.com>
2020-05-19 15:27:01 +0300

    Add template tag "show_menu" for rendering menu in _header and _footer

--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     """Category model for posts"""
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, verbose_name='Урл', unique=True)
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
 
     def __str__(self):
         return self.title
