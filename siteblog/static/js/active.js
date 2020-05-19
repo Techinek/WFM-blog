2fc64ea496a30af95ab307f9d4f59d877fdade05
techinek <techinek@gmail.com>
2020-05-19 16:10:27 +0300

    Fix hovering issues

--- siteblog/static/js/active.js
+++ siteblog/static/js/active.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
     $('.menu a').each(function(){
-        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
-        let link = this.href;
-        if(location == link){
-            $(this).parent().addClass('active');
-        }
+    let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+    let link = this.href;
+    if(location == link){
+        $(this).parent().addClass('active');
+    }
     });
 
     var filterFns = {
